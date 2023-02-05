0f15b140e802cb534854463bd111e8eee2f66acd
Mackenzie Leng <kenz.joy@outlook.com>
2023-02-04 22:38:31 -0700

    creates county serializer

--- app/controllers/api/v1/counties_controller.rb
+++ app/controllers/api/v1/counties_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CountiesController < ApplicationController
   def index
-    @counties = County.all 
-    render json: @counties
+    counties = County.all 
+    render json: CountySerializer.new(counties)
   end
 end
---
+++ app/serializers/county_serializer.rb
@@ -0,0 +1,4 @@
+class CountySerializer
+  include JSONAPI::Serializer
+  attributes :name
+end
