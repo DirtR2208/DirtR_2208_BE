7743717da994d4bc36769e6ef945aefc1720c7cd
Mackenzie Leng <kenz.joy@outlook.com>
2023-02-07 13:35:26 -0700

    add users table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_04_185933) do
+ActiveRecord::Schema.define(version: 2023_02_07_203420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2023_02_04_185933) do
     t.string "thumbnail_image"
     t.bigint "county_id"
     t.index ["county_id"], name: "index_trails_on_county_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "trails", "counties"
