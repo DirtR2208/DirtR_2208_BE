7c61ce50c488949f84ee7cfe52d30bf8c31429c7
Mackenzie Leng <kenz.joy@outlook.com>
2023-02-07 15:42:46 -0700

    tests passing for users index

--- spec/requests/api/v1/users/index_spec.rb
+++ spec/requests/api/v1/users/index_spec.rb
@@ -11,6 +11,26 @@ RSpec.describe 'GET /users' do
       get "/api/v1/users"
 
       users = JSON.parse(json_response, symbolize_names: true)
+
+      expect(users).to be_a Hash
+      expect(users).to have_key :data
+
+      expect(users[:data]).to be_a Array
+      expect(users[:data].first).to be_a Hash
+
+      expect(users[:data].first).to have_key :id
+      expect(users[:data].first).to have_key :type
+      expect(users[:data].first).to have_key :attributes
+
+      expect(users[:data].first[:id]).to be_a String
+      expect(users[:data].first[:type]).to be_a String
+      expect(users[:data].first[:attributes]).to be_a Hash
+
+      expect(users[:data].first[:attributes]).to have_key :name
+      expect(users[:data].first[:attributes]).to have_key :email
+
+      expect(users[:data].first[:attributes][:name]).to be_a String
+      expect(users[:data].first[:attributes][:email]).to be_a String
     end
   end
 end
