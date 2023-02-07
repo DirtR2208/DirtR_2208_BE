fd88320b2fce60bd533fa0efac0cf7da2efcdd72
Mackenzie Leng <kenz.joy@outlook.com>
2023-02-07 13:44:11 -0700

    all relationships have passing tests

--- spec/models/trail_spec.rb
+++ spec/models/trail_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Trail, type: :model do
   describe "Relationships" do
     it { should belong_to :county }
-    it { should belong_to :user }
+    it { should have_many :user_trails }
+    it { should have_many(:users).through(:user_trails) }
   end
 
   describe "Validations" do
