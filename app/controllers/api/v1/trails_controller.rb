627879d1076dcef53faee1f309e50a663a2dc9b8
Alex M <alexm0717@hotmail.com>
2023-02-07 12:21:20 -0800

    Am/complete trail show endpoint

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::TrailsController < ApplicationController
 
+  def show
+    @trail = Trail.find(params[:id])
+
+  end
 end
--- spec/requests/api/v1/trails/show_spec.rb
+++ spec/requests/api/v1/trails/show_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'GET /trails/:id' do
   describe "trails show" do
-    it 'should ' do
-
-    get "/api/v1/trails/#{:id}"
-    expect(response).to be_successful
+    before :each do
+      @boulder = County.create!( name: Faker::Name.name )
+      @trail1= @boulder.trails.create!(name: Faker::Mountain.name , latitude: Faker::Address.latitude, longitude:Faker::Address.longitude ,difficulty: Faker::Color.color_name, distance: Faker::Number.non_zero_digit, description: Faker::Lorem.sentence, start_elevation: Faker::Number.binary(digits: 4), avg_duration: "01:12" ,map_image: Faker::LoremFlickr.image(size: "50x60"), thumbnail_image: "trail test" )
+    end
+    it "returns a success response" do
+      get "/api/v1/trails/#{@trail1.id}"
+      binding.pry
+      expect(response).to be_successful
     end
   end
 end
+
+
 
 
