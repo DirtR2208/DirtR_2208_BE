d183b494441198aa22c92ad0051f6f174f31fb82
Mackenzie Leng <kenz.joy@outlook.com>
2023-02-07 20:21:29 -0700

    adds spec testing for find by user id

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::UsersController < ApplicationController
     users = User.all
     render json: UserSerializer.new(users)
   end
+
+  def show
+    
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
   end
+
+  describe 'class methods' do
+    before(:each) do
+      @reba = User.create!(name: 'Reba', email: 'reba@dirtr.dirt')
+    end
+
+    describe '#find_by_user_id(id)' do
+      it 'returns a user based on the id provided in the query' do
+        expect(User.find_by_user_id(@reba.id)).to eq(@reba)
+      end
+    end
+  end
 end
