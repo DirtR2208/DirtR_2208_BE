eb84c84c2cbe76b51989a86a0a48e3d12aa90569
Mackenzie Leng <kenz.joy@outlook.com>
2023-02-08 12:37:30 -0700

    happy path testing for user trails complete

--- spec/requests/api/v1/users/show_spec.rb
+++ spec/requests/api/v1/users/show_spec.rb
@@ -3,17 +3,31 @@ require 'rails_helper'
 RSpec.describe 'GET /user' do
   describe 'when the records exist' do
     it 'returns a specified user and their favorite trails in json' do
-      # json_response = File.read('spec/fixtures/DO_NOT_DELETE/shawn.json')
-      # stub_request(:get, "http://localhost:3000/api/v1/user?id=3").to_return(status: 200, body: json_response)
+      json_response = File.read('spec/fixtures/DO_NOT_DELETE/shawn.json')
+      stub_request(:get, "http://localhost:3000/api/v1/user?id=3").to_return(status: 200, body: json_response)
 
-      # shawn = JSON.parse(json_response, symbolize_names: true)
-      Rails.application.load_tasks
-      Rake::Task['csv_load:all'].invoke
-      get "/api/v1/user?id=3"
+      shawn = JSON.parse(json_response, symbolize_names: true)
+      # Rails.application.load_tasks
+      # Rake::Task['csv_load:all'].invoke
+      # get "/api/v1/user?id=3"
 
-      shawn = JSON.parse(response.body, symbolize_names: true)
-
+      # shawn = JSON.parse(response.body, symbolize_names: true)
+# require 'pry'; binding.pry
       expect(shawn).to be_a(Hash)
+      expect(shawn).to have_key(:data)
+      expect(shawn[:data]).to be_a(Hash)
+
+      expect(shawn[:data]).to have_key(:id)
+      expect(shawn[:data][:id]).to be_a(String)
+      expect(shawn[:data]).to have_key(:type)
+      expect(shawn[:data][:type]).to be_a(String)
+      expect(shawn[:data]).to have_key(:attributes)
+
+      expect(shawn[:data][:attributes]).to be_a(Hash)
+      expect(shawn[:data][:attributes]).to have_key(:name)
+      expect(shawn[:data][:attributes][:name]).to be_a(String)
+      expect(shawn[:data][:attributes]).to have_key(:trails)
+      expect(shawn[:data][:attributes][:trails]).to be_a(Array)
     end
   end
 end
