50c53e8a96cc63db7adff98cfd035e9c072af198
Mackenzie Leng <kenz.joy@outlook.com>
2023-02-09 13:04:09 -0700

    saving work to merge main

--- app/controllers/api/v1/user_trails_controller.rb
+++ app/controllers/api/v1/user_trails_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::UserTrailsController < ApplicationController
   def create
-    if UserTrail.create(user_id: params[:user_id], trail_id: params[:trail_id])
-      render json: { 'success': 'Trail Added Successfully.' }, status: 201
+    user = User.find_by(id: params[:id])
+    if user.present?
+      UserTrail.create(user_id: params[:user_id], trail_id: params[:trail_id])
+      render json: { 'success': 'Trail Added Successfully' }, status: 200
+    else 
+      render json: { 'error': 'No User Found'}
     end
   end
 
--- spec/requests/api/v1/user_trails/create_spec.rb
+++ spec/requests/api/v1/user_trails/create_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'POST /user-trails' do
-  describe 'when user id is valid' do
+  describe 'when user & trail id is valid' do
     it 'adds a trail to that users list of favorite trails' do
       shawn = User.create!(
         id: 3,  
@@ -38,7 +38,7 @@ RSpec.describe 'POST /user-trails' do
       post '/api/v1/user-trails', headers: headers, params: JSON.generate(body)
       
       expect(response).to be_successful
-      expect(response.status).to eq(201)
+      expect(response.status).to eq(200)
       
       parsed = JSON.parse(response.body, symbolize_names: true)
       
@@ -47,6 +47,49 @@ RSpec.describe 'POST /user-trails' do
       expect(parsed[:success]).to eq("Trail Added Successfully.")
 
       expect(shawn.trails.count).to eq(1)
+    end
+  end
+
+  describe 'when user id is not valid' do
+    it 'returns an error message' do
+      county = County.create!(
+        id: 27,
+        name: "Boulder"
+      )
+      trail = Trail.create!(
+        id: 163,
+        name: "Jenny Creek Trail ",
+        latitude: "39.929561",
+        longitude: "-105.624647",
+        difficulty: "black",
+        distance: "2.2",
+        description: "Jenny Creek Trail is a lesser-used trail that goes from Yankee Doodle Lake Eldora Ski area. In it motorized 2 track and in summer is an out-and-back from Rollins pass road or Jenny Creek Road with no summer access through Eldora.",
+        start_elevation: "10,732 ft",
+        avg_duration: "0:25:11",
+        map_image: "https://ep1.pinkbike.org/trailstaticmap/563000/563447_3_500x200.png",
+        thumbnail_image: "https://adventr.co/wp-content/uploads/2015/01/DSC03243.jpg",
+        county_id: 27
+      )
+
+      headers = { "Content-Type": "application/json", Accept: "application/json" }
+      body = ( {
+        "user_id":  nil,
+        "trail_id": 163
+      } )
+
+      post '/api/v1/user-trails', headers: headers, params: JSON.generate(body)
+      
+      expect(response).to be_successful
+      # require 'pry'; binding.pry
+      # expect(response.status).to eq(201)
+      
+      # parsed = JSON.parse(response.body, symbolize_names: true)
+      
+      # expect(parsed).to be_a(Hash)
+      # expect(parsed).to have_key(:success)
+      # expect(parsed[:success]).to eq("Trail Added Successfully.")
+
+
     end
   end
 end
