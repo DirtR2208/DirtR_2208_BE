82aac6816023729bd37f7c93862cd90393107be6
Mackenzie Leng <kenz.joy@outlook.com>
2023-02-09 15:29:42 -0700

    removes dead code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,16 +17,4 @@ class Api::V1::UsersController < ApplicationController
       render json: { error: "Provide A User Parameter" }, status: 404
     end
   end
-
-  # def add_favorite(trail)
-  #   require 'pry'; binding.pry
-  #   user = User.find_by(params[:id])
-  #   # if user.present?
-  # end
-
-  # private
-
-  # def trail_params
-  #   params.permit(:name)
-  # end
 end
