f82ed38fc7e088d93121d1b99933b15b8204805f
Alex M <alexm0717@hotmail.com>
2023-02-10 10:48:36 -0800

    delete user_trails

--- app/controllers/api/v1/user_trails_controller.rb
+++ app/controllers/api/v1/user_trails_controller.rb
@@ -7,11 +7,21 @@ class Api::V1::UserTrailsController < ApplicationController
       render json: { 'success': 'Trail Added Successfully' }, status: 200
     end
   end
+  def destroy
+    if user_trail.present?
+      user_trail.delete
+      render json: { 'success': 'Trail Remove from favorites' }, status: 200
+    else
+      render json:{ 'error': 'Trail not found'}, status: 404
+    end
+  end
 
   private
-
   def user_valid
     @user = User.find_by(params[:id])
     !@user.nil?
   end
+    def user_trail
+      UserTrail.find_by(params[:id])
+    end
 end
