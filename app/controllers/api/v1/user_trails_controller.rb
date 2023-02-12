94d83c6f5cbc614367b3e0166408c371e351bd11
Mackenzie Leng <kenz.joy@outlook.com>
2023-02-12 11:16:37 -0700

    optimize user trails create action

--- app/controllers/api/v1/user_trails_controller.rb
+++ app/controllers/api/v1/user_trails_controller.rb
@@ -1,17 +1,23 @@
 class Api::V1::UserTrailsController < ApplicationController
   def create
-    if !user_valid
-      render json: { 'error': 'No User Found' }, status: 200
-    else 
-      UserTrail.create(user_id: params[:user_id], trail_id: params[:trail_id])
+    user_trail = UserTrail.new(user_id: params[:user_id], trail_id: params[:trail_id])
+    if user_trail.save
       render json: { 'success': 'Trail Added Successfully' }, status: 200
+    else 
+      render json: { 'error': 'No User Found' }, status: 200
     end
+    # if !user_valid
+    #   render json: { 'error': 'No User Found' }, status: 200
+    # else 
+    #   UserTrail.create(user_id: params[:user_id], trail_id: params[:trail_id])
+    #   render json: { 'success': 'Trail Added Successfully' }, status: 200
+    # end
   end
 
-  private
+  # private
 
-  def user_valid
-    @user = User.find_by(params[:id])
-    !@user.nil?
-  end
+  # def user_valid
+  #   @user = User.find_by(params[:id])
+  #   !@user.nil?
+  # end
 end
